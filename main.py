10f693bdca1ad97feb400a0b0ad4ddfe2513d920
Jentos2020 <bio3d@mail.ru>
2023-05-27 23:53:16 +0700

    Made the basis of the bot

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+import os
+import asyncio
+from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from keyboards import setMainKb
+from handlers import registerUserHandlers
+
+storage = MemoryStorage()
+
+
+def register_handler(dp: Dispatcher):
+    registerUserHandlers(dp)
+
+async def main():
+    load_dotenv()
+    bot = Bot(os.getenv('TOKEN_API'))
+    dp = Dispatcher(bot, storage=storage)
+    registerUserHandlers(dp)
+    try:
+        await dp.skip_updates()
+        await dp.start_polling()
+    except Exception as ex:
+        print(ex)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
